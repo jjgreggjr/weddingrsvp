37d443dfbc1fabd7a99658d02d0c6745c2abff4d
jjgreggjr <james@searchclickboom.com>
2026-01-10 15:42:24 -0700

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const CONFIG = {
 
     // --- HERO SECTION ---
     hero: {
-        title: "JJ & CCC",
+        title: "JJG & CCC",
         date: "June 19, 2027",
         targetDate: "June 19, 2027 16:00:00 GMT-0600" // Date for Countdown
     },
