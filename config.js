a515cf51d394726c1265a62459ecaf9fdf458579
jjgreggjr <james@searchclickboom.com>
2026-01-10 15:10:29 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// Edit this file to change your site settings!
+const CONFIG = {
+    // Your Colors
+    mainColor: "#7b7f68",
+    accentColor: "#223346",
+    
+    // Images (Use the file name if uploaded to GitHub, or a full URL)
+    heroImage: "hero.jpg", 
+    
+    // Text
+    dateText: "August 24, 2026",
+    venueName: "The Mountain Overlook",
+    
+    // Links
+    registryLink: "https://www.zola.com"
+};
